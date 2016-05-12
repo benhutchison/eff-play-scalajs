7f21b89a945f9a5d1e18feb3ce59507f64d835a7
Ben Hutchison <brhutchison@gmail.com>
2016-05-12 14:53:45 +1000

    add reader, still works

--- shared/src/main/scala/effplay/EffPlay.scala
+++ shared/src/main/scala/effplay/EffPlay.scala
@@ -8,14 +8,14 @@ object EffPlay extends App {
   type StateInt[R] = Member[State[Int, ?], R]
   type WriterString[R] = Member[Writer[String, ?], R]
 
-  type E = State[Int, ?] |: Writer[String, ?] |: NoEffect
+  type E = State[Int, ?] |: Writer[String, ?]  |: Reader[String, ?]|: NoEffect
 
-  def putAndTell[R : StateInt : WriterString](i: Int): Eff[R, Int] =
+  def putAndTell[R : StateInt : WriterString](i: Int) =
     for {
       _ <- put(i)
       _ <- tell("stored " + i)
     } yield i
 
-  putAndTell[E](7).runState(0).runWriter.run
+  putAndTell[E](7).runState(0).runWriter.runReader("").run
 
 }
